33566c20db6afcb9dcb99dda5bba0488f9add57d
jackrude <jackrude@Jacks-MacBook-Pro-2.local>
2017-03-20 20:38:09 -1000

    Final Mockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -14,6 +14,26 @@ FlowRouter.route('/list', {
     BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
   },
 });
+FlowRouter.route('/login', {
+  name: 'Login_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Login_Page' });
+  },
+});
+
+FlowRouter.route('/view-profiles', {
+  name: 'View_Profiles_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'View_Profiles_Page' });
+  },
+});
+
+FlowRouter.route('/edit-profile-user', {
+  name: 'Edit_Profile_User_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Profile_User_Page' });
+  },
+});
 
 FlowRouter.route('/add', {
   name: 'Add_Stuff_Page',
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,8 +1,16 @@
 import './add-stuff-page.html';
 import './add-stuff-page.js';
 
-import './edit-stuff-page.html';
-import './edit-stuff-page.js';
+import './edit-profile-user-page.html';
+import './edit-profile-user-page.js';
+
+import './view-profiles-page.html';
+import './view-profiles-page.js';
+
+import './login-page.html';
+import './login-page.js';
+
+
 
 import './home-page.html';
 
